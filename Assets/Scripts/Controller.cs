0fdd7f8cd220a85bb7b6bcf092eae0fe06404f76
ArakiTakaki <araki_takaki@yahoo.co.jp>
2019-12-08 19:33:54 +0900

    Unityでブロック崩しゲーム的なものを作成した。

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // left arrw ( <- )
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            // 対象となるオブジェクトのポジションが左へずれる？
+            // transform forwardが謎
+            transform.position -= transform.forward * 0.2f;
+            // 対象となるオブジェクトのポジションが右へずれる？
+            // transform forwardが謎
+        }else if (Input.GetKey(KeyCode.RightArrow)) {
+            transform.position += transform.forward * 0.2f;
+        }
+    }
+}
---
+++ Assets/Scripts/KabeOut.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class KabeOut : MonoBehaviour {
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+    
+
+    private void OnCollisionEnter(Collision collision) {
+        GameObject.Find("Master").GetComponent<GameMaster>().GameOver("ゲーム失敗．また挑戦しよう"); //<=====文字列を引数へ
+    }
+}
+
